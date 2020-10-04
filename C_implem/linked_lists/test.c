9af4f6df94cf47b15f5a0b669ba6830500fc2c11
hugo <hugo.francon@ens-rennes.fr>
2020-10-04 18:11:54 +0200

    Now tests insert_head, append and empty

--- C_implem/linked_lists/test.c
+++ C_implem/linked_lists/test.c
@@ -1,9 +1,19 @@
 #include "linked_lists.h"
 
 int main(int argc, char** argv) {
-    int_list_t* list = create_empty_int_list();
+    int_list_t* list1 = create_empty_int_list();
+    int_list_t* list2 = create_empty_int_list();
+    int_list_t* list3 = create_empty_int_list();
+
     for (int i = 0; i <= 100; i++) {
-        append_int_list(i, list);
+        insert_head_int_list(i, list1);
+        insert_head_int_list(i, list2);
+        append_int_list(i, list3);
     }
-    print_int_list(list);
+    // destroy_int_list(list1);
+    empty_int_list(list2);
+
+    print_int_list(list1); // Should print [ 100; 99; ... ; 0 ]
+    print_int_list(list2); // Should print [ ]
+    print_int_list(list3); // Should print [ 0; 1; ... ; 100 ]
 }
