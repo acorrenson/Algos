e9a40c92cad5b91c98af4946e89250166fa4c523
Théo Degioanni <moxinilian@tutanota.com>
2020-10-24 09:13:49 +0200

    more explicit method names

--- Rust_implem/src/binary_heap.rs
+++ Rust_implem/src/binary_heap.rs
@@ -15,20 +15,20 @@ impl<K: Ord, V> BinaryHeapVec<K, V> {
         Self { data: Vec::new() }
     }
 
-    fn fix_at(&mut self, index: usize) {
+    fn heapify_up_at(&mut self, index: usize) {
         if index != 0 {
-            let (k, _) = self.data.get(index).expect("invalid index to fix at");
+            let (k, _) = self.data.get(index).expect("invalid index to heapify at");
 
             let parent = (index - 1) / 2;
             let (parent_k, _) = self.data.get(parent).unwrap();
             if k < parent_k {
                 self.data.swap(index, parent);
-                self.fix_at(parent);
+                self.heapify_up_at(parent);
             }
         }
     }
 
-    fn fix_down_at(&mut self, index: usize) {
+    fn heapify_down_at(&mut self, index: usize) {
         if let Some((k_self, _)) = self.data.get(index) {
             let c1 = 2 * index + 1;
             let c2 = 2 * index + 2;
@@ -39,17 +39,17 @@ impl<K: Ord, V> BinaryHeapVec<K, V> {
                     if k_self > k1 || k_self > k2 {
                         if k1 < k2 {
                             self.data.swap(index, c1);
-                            self.fix_down_at(c1);
+                            self.heapify_down_at(c1);
                         } else {
                             self.data.swap(index, c2);
-                            self.fix_down_at(c2);
+                            self.heapify_down_at(c2);
                         }
                     }
                 } else {
                     // The current index only has one child
                     if k_self > k1 {
                         self.data.swap(index, c1);
-                        self.fix_down_at(c1);
+                        self.heapify_down_at(c1);
                     }
                 }
             }
@@ -65,7 +65,7 @@ impl<K: Ord, V> BinaryHeap<K, V> for BinaryHeapVec<K, V> {
     fn insert(&mut self, key: K, value: V) {
         let index = self.data.len();
         self.data.push((key, value));
-        self.fix_at(index);
+        self.heapify_up_at(index);
     }
 
     fn peek_min(&self) -> Option<(&K, &V)> {
@@ -77,7 +77,7 @@ impl<K: Ord, V> BinaryHeap<K, V> for BinaryHeapVec<K, V> {
             let last = self.data.len() - 1;
             self.data.swap(0, last);
             let res = self.data.pop();
-            self.fix_down_at(0);
+            self.heapify_down_at(0);
             res
         } else {
             None
@@ -171,7 +171,7 @@ impl<K: Ord, V> BinaryHeapRec<K, V> {
         }
     }
 
-    fn fix_down(&mut self) {
+    fn heapify_down(&mut self) {
         match self {
             Self::Nil => (),
 
@@ -189,7 +189,7 @@ impl<K: Ord, V> BinaryHeapRec<K, V> {
             } => {
                 if left.is_key_smaller_than(key) {
                     left.swap(key, value);
-                    left.fix_down();
+                    left.heapify_down();
                 }
             }
 
@@ -203,10 +203,10 @@ impl<K: Ord, V> BinaryHeapRec<K, V> {
                 if left.is_key_smaller_than(key) || right.is_key_smaller_than(key) {
                     if left.is_smaller_than(right) {
                         left.swap(key, value);
-                        left.fix_down();
+                        left.heapify_down();
                     } else {
                         right.swap(key, value);
-                        right.fix_down();
+                        right.heapify_down();
                     }
                 }
             }
@@ -382,7 +382,7 @@ impl<K: Ord, V> BinaryHeap<K, V> for BinaryHeapRec<K, V> {
             }
         };
 
-        self.fix_down();
+        self.heapify_down();
 
         res
     }
