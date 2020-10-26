0642c037b80a7ae9ee5982774141e23df5be1a53
hugo <hugo.francon@ens-rennes.fr>
2020-10-26 16:50:08 +0100

    Added topological sort

--- OCaml_implem/topological_sort/topological_sort.ml
+++ OCaml_implem/topological_sort/topological_sort.ml
@@ -13,3 +13,21 @@ let dfs graph pre post =
       end
   in 
   Array.iteri explore graph
+
+(* Returns the sorted list of the nodes in the graph and an ordering function *)
+let topological_sort graph = 
+  let order_list = ref [] in
+  let post i = order_list := i::(!order_list) in
+  let _ = dfs graph (fun _ -> ()) post in
+  let sorted_list = !order_list in
+  let order_fun i j = 
+    let rec order_fun_aux i j = 
+      function
+      | [] -> failwith "order_fun : Nodes i and j are not in the graph"
+      | h::_ when h = i -> true
+      | h::_ when h = j -> false
+      | _::t -> order_fun_aux i j t
+    in
+    order_fun_aux i j sorted_list
+  in
+  (sorted_list,order_fun)
