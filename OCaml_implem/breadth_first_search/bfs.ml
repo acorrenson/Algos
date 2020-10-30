0b02832bf4e7200b757da8116ceb71100a26d110
hugo <hugo.francon@ens-rennes.fr>
2020-10-30 14:02:05 +0100

    Added the visited array to prevent multiple visits

--- OCaml_implem/breadth_first_search/bfs.ml
+++ OCaml_implem/breadth_first_search/bfs.ml
@@ -21,6 +21,7 @@ let distance_order_fun distance1 distance2 =
 
 let dijkstra graph source =
   let n = Array.length graph in
+  let visited = Array.make n false in
   if source >= n then failwith "Source of Dijkstra's Algorithm is not in the graph"
   else begin
     let distance_array = Array.make n (Infinite) in 
@@ -29,15 +30,18 @@ let dijkstra graph source =
     let pred = Array.make n (None) in
     while not(Heap.is_empty prio_queue) do 
       let (closest_distance,closest) = Heap.extract prio_queue in 
-      List.iter (fun (weight,node) -> 
-          let new_distance = closest_distance + weight in
-          if distance_order_fun distance_array.(node) (Finite new_distance)
-          then begin
-            distance_array.(node) <- Finite new_distance;
-            Heap.insert prio_queue (new_distance,node);
-            pred.(node) <- Some closest;
-          end)
-        graph.(closest)
+      if not(visited.(closest)) then begin
+        List.iter (fun (weight,node) -> 
+            let new_distance = closest_distance + weight in
+            if distance_order_fun distance_array.(node) (Finite new_distance)
+            then begin
+              distance_array.(node) <- Finite new_distance;
+              Heap.insert prio_queue (new_distance,node);
+              pred.(node) <- Some closest;
+            end)
+          graph.(closest);
+        visited.(closest) <- true
+      end
     done;
     (distance_array,pred)
   end
